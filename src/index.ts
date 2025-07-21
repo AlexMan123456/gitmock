a308957b4be3e95f3be3741e06e15c136f365235
AlexMan123456 <alex.man03888@gmail.com>
2025-07-22 00:37:11 +0100

    Make constructor private

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ class GitTestClient {
   private directory: string;
   repository: string;
 
-  constructor(directory: string) {
+  private constructor(directory: string) {
     this.directory = directory;
     this.repository = path.resolve(this.directory, "test-repository");
   }
@@ -15,18 +15,22 @@ class GitTestClient {
     return await execa(command, args, { ...options, cwd: this.repository });
   }
 
-  async setup(initialFileName: string = "README.md") {
-    const originDirectory = path.join(this.directory, "origin.git");
+  static async create(
+    directory: string,
+    initialFileName: string = "README.md",
+  ) {
+    const client = new GitTestClient(directory);
+    const originDirectory = path.join(client.directory, "origin.git");
     await execa("git", ["init", "--bare", originDirectory]);
-    await execa("mkdir", ["test-repository"], { cwd: this.directory });
-    await this.run("git", ["init"]);
-    await this.run("git", ["checkout", "-b", "main"]);
-    await this.run("git", ["remote", "add", "origin", originDirectory]);
-    const testFilePath = path.join(this.repository, initialFileName);
+    await execa("mkdir", ["test-repository"], { cwd: client.directory });
+    await client.run("git", ["init"]);
+    await client.run("git", ["checkout", "-b", "main"]);
+    await client.run("git", ["remote", "add", "origin", originDirectory]);
+    const testFilePath = path.join(client.repository, initialFileName);
     await writeFile(testFilePath, "");
-    await this.run("git", ["add", "."]);
-    await this.run("git", ["commit", "-m", "Initial commit"]);
-    await this.run("git", ["push", "origin", "main"]);
+    await client.run("git", ["add", "."]);
+    await client.run("git", ["commit", "-m", "Initial commit"]);
+    await client.run("git", ["push", "origin", "main"]);
   }
 }
 
