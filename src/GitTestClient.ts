f3a8bd0ac7196bebb019b35458284eda3344a316
AlexMan123456 <alex.man03888@gmail.com>
2025-07-24 23:46:20 +0100

    Add extra possible repository options

--- src/GitTestClient.ts
+++ src/GitTestClient.ts
@@ -18,20 +18,40 @@ class GitTestClient {
   static async create(
     directory: string,
     initialFileName: string = "README.md",
+    initialCommitMessage: string = "Initial commit",
   ) {
-    const client = new GitTestClient(directory);
-    const originDirectory = path.join(client.directory, "origin.git");
+    const gitTestClient = new GitTestClient(directory);
+    await execa(
+      "git",
+      ["config", "--global", "user.email", "test@example.com"],
+      {
+        env: {
+          HOME: gitTestClient.directory,
+        },
+      },
+    );
+    await execa("git", ["config", "--global", "user.name", "Test User"], {
+      env: {
+        HOME: gitTestClient.directory,
+      },
+    });
+    const originDirectory = path.join(gitTestClient.directory, "origin.git");
     await execa("git", ["init", "--bare", originDirectory]);
-    await execa("mkdir", ["test-repository"], { cwd: client.directory });
-    await client.run("git", ["init"]);
-    await client.run("git", ["checkout", "-b", "main"]);
-    await client.run("git", ["remote", "add", "origin", originDirectory]);
-    const testFilePath = path.join(client.repository, initialFileName);
+    await execa("mkdir", ["test-repository"], { cwd: gitTestClient.directory });
+    await gitTestClient.run("git", ["init"]);
+    await gitTestClient.run("git", ["checkout", "-b", "main"]);
+    await gitTestClient.run("git", [
+      "remote",
+      "add",
+      "origin",
+      originDirectory,
+    ]);
+    const testFilePath = path.join(gitTestClient.repository, initialFileName);
     await writeFile(testFilePath, "");
-    await client.run("git", ["add", "."]);
-    await client.run("git", ["commit", "-m", "Initial commit"]);
-    await client.run("git", ["push", "origin", "main"]);
-    return client;
+    await gitTestClient.run("git", ["add", "."]);
+    await gitTestClient.run("git", ["commit", "-m", initialCommitMessage]);
+    await gitTestClient.run("git", ["push", "origin", "main"]);
+    return gitTestClient;
   }
 }
 
--- src/repository-task.ts
+++ src/repository-task.ts
@@ -1,12 +1,21 @@
 import GitTestClient from "src/GitTestClient";
 import { type DirectoryOptions, temporaryDirectoryTask } from "tempy";
 
+export interface RepositoryOptions extends DirectoryOptions {
+  initialFileName?: string;
+  initialCommitMessage?: string;
+}
+
 function repositoryTask(
   taskFunction: (gitTestClient: GitTestClient) => Promise<void>,
-  options?: DirectoryOptions,
+  options?: RepositoryOptions,
 ) {
   return temporaryDirectoryTask(async (temporaryDirectory) => {
-    const gitTestClient = await GitTestClient.create(temporaryDirectory);
+    const gitTestClient = await GitTestClient.create(
+      temporaryDirectory,
+      options?.initialFileName,
+      options?.initialCommitMessage,
+    );
     await taskFunction(gitTestClient);
   }, options);
 }
