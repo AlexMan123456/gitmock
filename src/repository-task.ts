966da8c5733e6dbe4c0f541dd73a76e8f8512aba
AlexMan123456 <alex.man03888@gmail.com>
2025-07-25 23:00:54 +0100

    Add missing async/await

--- src/repository-task.ts
+++ src/repository-task.ts
@@ -3,11 +3,11 @@ import { type DirectoryOptions, temporaryDirectoryTask } from "tempy";
 
 export type RepositoryOptions = InitialSetupOptions & DirectoryOptions;
 
-function repositoryTask(
+async function repositoryTask(
   taskFunction: (gitTestClient: GitTestClient) => Promise<void>,
   options?: RepositoryOptions,
 ) {
-  return temporaryDirectoryTask(async (temporaryDirectory) => {
+  return await temporaryDirectoryTask(async (temporaryDirectory) => {
     const gitTestClient = await GitTestClient.create(
       temporaryDirectory,
       options,
